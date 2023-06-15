ffdb20d4ece0e540f8b69766a3a1530a2f1b6e17
JungWooojin <130621526+JungWooojin@users.noreply.github.com>
2023-06-15 21:15:36 +0900

    Add files via upload

---
+++ main/java/com/green/shoppingmall/product/model/ProductInsDto.java
@@ -0,0 +1,11 @@
+package com.green.shoppingmall.product.model;
+
+import lombok.Data;
+
+@Data
+public class ProductInsDto {
+    private int price;
+    private String ctnt;
+    private String nm;
+    private String brand;
+}
