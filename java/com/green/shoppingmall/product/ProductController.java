3e1d777f7c8dac280457a7108868e98188052fcf
JungWooojin <130621526+JungWooojin@users.noreply.github.com>
2023-06-16 15:01:43 +0900

    Add files via upload

---
+++ java/com/green/shoppingmall/product/ProductController.java
@@ -0,0 +1,58 @@
+package com.green.shoppingmall.product;
+
+import com.green.shoppingmall.product.model.ProductEntity;
+import com.green.shoppingmall.product.model.ProductInsDto;
+import com.green.shoppingmall.product.model.ProductVo;
+import com.green.shoppingmall.product.model.SingSangSongDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    private final ProductService service;
+
+    @Autowired
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE
+            , MediaType.APPLICATION_JSON_VALUE })
+    public int insProduct(@RequestPart MultipartFile img
+            , @RequestPart ProductInsDto dto) {
+        return service.insProduct(img, dto);
+    }
+
+    @PostMapping(value="/{iproduct}", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public Long insProductPics(@PathVariable Long iproduct, @RequestPart List<MultipartFile> pics) throws Exception{
+        return service.insProductPics(iproduct, pics);
+
+    }
+
+
+
+
+    //연습했음
+    @PostMapping(value="/file", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public int singSangSong(@RequestPart MultipartFile file, @RequestPart SingSangSongDto data) {
+        System.out.println(data);
+        System.out.println(file.getOriginalFilename());
+        return 0;
+    }
+
+
+    @RequestMapping(value="/test1", method = RequestMethod.GET)
+    public String test1() {
+        return "test1";
+    }
+    @GetMapping
+    public List<ProductVo> getProduct() {
+        return service.selProduct();
+    }
+
+}
---
+++ java/com/green/shoppingmall/product/ProductService.java
@@ -0,0 +1,102 @@
+package com.green.shoppingmall.product;
+
+import com.green.shoppingmall.product.model.ProductEntity;
+import com.green.shoppingmall.product.model.ProductInsDto;
+import com.green.shoppingmall.product.model.ProductPicEntity;
+import com.green.shoppingmall.product.model.ProductVo;
+import com.green.shoppingmall.utils.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Value("${file.dir}")
+    private String fileDir;
+
+    private final ProductMapper mapper;
+
+    @Autowired
+    public ProductService(ProductMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public int insProduct(MultipartFile img, ProductInsDto dto) {
+        ProductEntity entity = new ProductEntity();
+        entity.setPrice(dto.getPrice());
+        entity.setCtnt(dto.getCtnt());
+        entity.setNm(dto.getNm());
+        entity.setBrand(dto.getBrand());
+
+        //1. 랜덤한 파일명 만든다.
+        String savedFileNm = FileUtils.makeRandomFileNm(img.getOriginalFilename());
+        System.out.println("savedFileNm : " + savedFileNm);
+
+        //2. insert한다
+        entity.setMainPic(savedFileNm);
+        int result = mapper.insProduct(entity);
+
+        //3. pk값 얻는다
+        System.out.println("result : " + result);
+        System.out.println("pk : " + entity.getIproduct());
+
+        //4. D:/download/shoppingmall/product/pk값 폴더 생성
+        String targetDir = String.format("%s/product/%d", fileDir, entity.getIproduct());
+        File fileTargetDir = new File(targetDir);
+        if(!fileTargetDir.exists()) {
+            fileTargetDir.mkdirs();
+        }
+
+        //5. 이미지를 해당 폴더로 옮긴다.
+        //File fileTarget = new File(targetDir + "/" + savedFileNm);
+        File fileTarget = new File(String.format("%s/%s", targetDir, savedFileNm));
+        try {
+            img.transferTo(fileTarget);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 1;
+    }
+
+
+    public Long insProductPics(Long iproduct, List<MultipartFile> pics) throws Exception {
+        // pk > pics 폴더를 만든다.
+        String targetDir = String.format("%s/product/%d/pics", fileDir, iproduct);
+        File fileTargetDir = new File(targetDir);
+        if(!fileTargetDir.exists()) {
+            fileTargetDir.mkdirs();
+        }
+
+        List<ProductPicEntity> picList = new ArrayList<>();
+
+        for(MultipartFile img : pics) {
+            String savedFileNm = FileUtils.makeRandomFileNm(img.getOriginalFilename());
+            System.out.println("savedFileNm : " + savedFileNm); //파일명 만든다
+
+            File fileTarget = new File(String.format("%s/%s", targetDir, savedFileNm)); //객체만든다
+            try {
+                img.transferTo(fileTarget); //해당하는곳으로 파일이동
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new Exception("이미지 저장 실패"); //예외발생시
+            }
+
+            ProductPicEntity entity = new ProductPicEntity(); //db에 저장할 값저장을 위한 아이템 생성
+            entity.setIproduct(iproduct);
+            entity.setPic(savedFileNm);
+            picList.add(entity);
+        }
+
+        return Long.valueOf(mapper.insProductPic(picList));
+    }
+    List<ProductVo> selProduct() {
+        return mapper.selProduct();
+    }
+}
